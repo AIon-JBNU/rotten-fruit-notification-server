01b02ab02d90e81ee82dd4db91cbcaf60e1c6288
songbuild00 <songbuild00@gmail.com>
2023-12-17 19:30:29 +0900

    fix: Firebase 관련 정보 감추기

--- src/main/kotlin/com/example/rottenfruitnotiserver/FCMConfig.kt
+++ src/main/kotlin/com/example/rottenfruitnotiserver/FCMConfig.kt
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
@@ -11,9 +12,12 @@ import java.io.InputStream
 
 @Configuration
 class FCMConfig {
+    @Value("\${app.firebase-configuration-file}")
+    lateinit var firebaseConfigPath: String
+
     @Bean
     fun firebaseMessaging(): FirebaseMessaging {
-        val resource = ClassPathResource("firebase/capstone-project-406412-firebase-adminsdk-obpon-015a448923.json")
+        val resource = ClassPathResource(firebaseConfigPath)
 
         val refreshToken = resource.inputStream
 
